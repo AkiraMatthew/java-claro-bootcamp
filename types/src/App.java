c1bb0c1defb8cac2901067eab672bf8f59a31bf2
Akira <akira.dev.math@gmail.com>
2024-08-20 16:23:43 -0300

    end of module

--- types/src/App.java
+++ types/src/App.java
@@ -1,5 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double sM = 2500;
+        
+        short nC = 1;
+        int nN = nC;
+        short nC2 = (short)nN;
+
+        int numero = 1;
+        numero = 2;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        VALOR_DE_PI = 10;
     }
 }
