e5de14fa4da31ab8e6188702b188aa73bf05da3c
Akira <akira.dev.math@gmail.com>
2024-09-03 21:18:35 -0300

    addMusic and generic ReprodutorMusical methods implementation in main class

---
+++ Modelagem-Diagramacao-Componente-Iphone/src/com/phone/model/Iphone.java
@@ -0,0 +1,57 @@
+package com.phone.model;
+
+import com.phone.service.ModuloTelefonico;
+import com.phone.service.NavegadorWeb;
+import com.phone.service.ReprodutorMusical;
+
+import java.util.Scanner;
+
+public class Iphone implements ModuloTelefonico, NavegadorWeb, ReprodutorMusical {
+    Scanner terminal = new Scanner(System.in);
+
+    @Override
+    public boolean play() {
+        return false;
+    }
+
+    @Override
+    public boolean pause() {
+        return false;
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public String[] addMusic(String music) {
+        String[] choosenMusics = {""};
+        boolean chooseMusic = true;
+        do{
+            for(int i = 0; i < choosenMusics.length; i++){
+                System.out.println("Digite o nome da música a ser adicionada");
+                music = terminal.nextLine();
+                choosenMusics = new String[]{music + choosenMusics[i]};
+
+                System.out.println("Deseja adicionar outra música? (sim/nao): ");
+                String response = scanner.nextLine().trim().toLowerCase();
+                // Handle the response
+                if (response.equals("sim")) {
+                    System.out.println("Novamente...");
+                    // Continue with the operation
+                } else if (response.equals("nao")) {
+                    System.out.println("Você está saindo da aba de adicionar músicas.");
+                    chooseMusic = false;
+                    break;
+                    // Handle the denial
+                } else {
+                    System.out.println("Resposta inválida. Somente (sim/nao) ");
+                    // Optionally, you can loop back and ask again
+                }
+            }
+        }while(chooseMusic);
+
+        return choosenMusics;
+    }
+}
---
+++ Modelagem-Diagramacao-Componente-Iphone/src/com/phone/service/ReprodutorMusical.java
@@ -0,0 +1,8 @@
+package com.phone.service;
+
+public interface ReprodutorMusical {
+    void play();
+    boolean pause();
+    void stop();
+    String[] addMusic();
+}
