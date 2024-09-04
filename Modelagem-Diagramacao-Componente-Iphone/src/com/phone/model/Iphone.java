d8e6779b9e1801aa29676636ff577c16a0c6bfde
Akira <akira.dev.math@gmail.com>
2024-09-03 21:53:31 -0300

    update

--- Modelagem-Diagramacao-Componente-Iphone/src/com/phone/model/Iphone.java
+++ Modelagem-Diagramacao-Componente-Iphone/src/com/phone/model/Iphone.java
@@ -9,34 +9,31 @@ import java.util.Scanner;
 public class Iphone implements ModuloTelefonico, NavegadorWeb, ReprodutorMusical {
     Scanner terminal = new Scanner(System.in);
 
-    @Override
-    public boolean play() {
-        return false;
+    //Music Player methods
+    public void play() {
     }
 
-    @Override
     public boolean pause() {
         return false;
     }
 
-    @Override
     public void stop() {
 
     }
 
     //This is a demonstrative method to simulate a user adding a music through terminal interaction
-    public String[] addMusic(String music) {
+    public String[] addMusic() {
         String[] choosenMusics = {""};
         //chooseMusic = true means that the user is on the music registration menu
         boolean chooseMusic = true;
         do{
             for(int i = 0; i < choosenMusics.length; i++){
                 System.out.println("Digite o nome da música a ser adicionada");
-                music = terminal.nextLine();
+                String music = terminal.nextLine();
                 choosenMusics = new String[]{music + choosenMusics[i]};
 
                 System.out.println("Deseja adicionar outra música? (sim/nao): ");
-                String response = scanner.nextLine().trim().toLowerCase();
+                String response = terminal.nextLine().trim().toLowerCase();
                 // Handle the response
                 if (response.equals("sim")) {
                     System.out.println("Novamente...");
@@ -54,5 +51,37 @@ public class Iphone implements ModuloTelefonico, NavegadorWeb, ReprodutorMusical
         }while(chooseMusic);
 
         return choosenMusics;
+    }
+
+    // Cellphone calls methods
+
+    public void call() {
+
+    }
+
+    public void answerCall() {
+
+    }
+
+    public String[] saveNumber() {
+        return new String[0];
+    }
+
+    public void deleteNumber() {
+
+    }
+
+    // Web Navigator Methods
+
+    public void openTab() {
+
+    }
+
+    public void closeTab() {
+
+    }
+
+    public String search() {
+        return "";
     }
 }
