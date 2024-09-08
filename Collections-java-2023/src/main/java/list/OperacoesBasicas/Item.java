c029b23a3b6354c24180571972dd2378d00882fe
Akira <akira.dev.math@gmail.com>
2024-09-08 19:55:22 -0300

    update

--- Collections-java-2023/src/main/java/list/OperacoesBasicas/Item.java
+++ Collections-java-2023/src/main/java/list/OperacoesBasicas/Item.java
@@ -1,4 +1,33 @@
 package main.java.list.OperacoesBasicas;
 
 public class Item {
+    private String productName;
+    private double productPrice;
+    private int productQuantity;
+
+    public Item(String productName, double productPrice, int productQuantity){
+        this.productName = productName;
+        this.productPrice= productPrice;
+        this.productQuantity = productQuantity;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public int getProductQuantity() {
+        return productQuantity;
+    }
+
+    public String getProductInfo() {
+        String productInfo = (productName + productPrice + productQuantity);
+        return productInfo;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto: " + productName + '\'' +
+                "Preço: " + productPrice +
+                "Quantia: " + productQuantity;
+    }
 }
